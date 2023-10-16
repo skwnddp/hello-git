e629c8668c5075ddd685d0a5d0f80548e1d0d2d0
kmj <7883121@naver.com>
2023-10-16 04:05:25 +0000

    C4 : modify hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 // Hello.java
 public class Hello {
    public static void main(String[] args) {
-      System.out.println("Hello, world from GIT!");
+        System.out.println("Hello, world from GIT!");
         system.out.println("Change after First commit!");
+        System.out.println("gogo");
   }
 }
